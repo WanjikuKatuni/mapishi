badce72e973ccad2213a91fd7b7724c083064c12
wanjiku <annritawanjiku77@gmail.com>
2023-02-28 15:16:59 +0300

    setup index, show, new and create methods

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -2,4 +2,16 @@ class RecipesController < ApplicationController
     # index method/action
     def index
     end
+
+    #show recipes
+    def show
+    end
+
+    #new recipes
+    def new
+    end
+
+    #create recipes
+    def create
+    end
 end
